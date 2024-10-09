00eb26d67d686a2057b4ded14ac2df7e373fa0ba
ISABELLA ALVES DA SILVA LIMA TAVARES <isabella.asltavares@senacsp.edu.br>
2024-10-09 17:31:54 -0300

    falta 1 erro de boas praticas isabella

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,23 +1,23 @@
 import { Text, View, StyleSheet, Image } from 'react-native';
-
+ 
 export default function Icon() {
   return (
-    <View style={styles.container}>      
-      <Image style={styles.logo} source={require('/assets/snack-icon.png')} />
-      <Text style={styles.paragraph}>
+<View style={styles.container}>      
+<Image style={styles.logo} source={require('../assets/snack-icon.png')} />
+<Text style={styles.paragraph}>
         MEU APP
-      </Text>
-    </View>
+</Text>
+</View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     justifyContent: 'center',
-    paddingVertical:24,
-    backgroundColor:'#ccc',
-    width:'100%'
+    paddingVertical: 24,
+    backgroundColor: '#ccc',
+    width: '100%',
   },
   paragraph: {
     margin: 24,
@@ -29,5 +29,5 @@ const styles = StyleSheet.create({
   logo: {
     height: 128,
     width: 128,
-  }
+  },
 });
